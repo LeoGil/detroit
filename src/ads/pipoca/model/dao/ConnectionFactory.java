88548592b797ecf6d939dfcc27c53b925146d665
Leonardo Gil <leonardo.lgc@policiacientifica.sp.gov.br>
2020-05-16 20:10:30 -0300

    atualizacao bootstrap

--- src/ads/pipoca/model/dao/ConnectionFactory.java
+++ src/ads/pipoca/model/dao/ConnectionFactory.java
@@ -1,4 +1,5 @@
 package ads.pipoca.model.dao;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,33 +9,33 @@ public class ConnectionFactory {
 	static {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
+
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
 	}
 
-	//Conexao Gil 1
+	// Conexao Gil 1
+//	public static Connection getConnection() throws IOException {
+//		try {
+//			return DriverManager .getConnection("jdbc:mysql://localhost:3306/detroit?"
+//					+ "useTimezone=true&serverTimezone=UTC&user=root&password=rootp@ssw0rd");
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			throw new IOException(e);
+//		}
+//	}
+//	
+	// Conexao Gil 2
 	public static Connection getConnection() throws IOException {
 		try {
-			return DriverManager .getConnection("jdbc:mysql://localhost:3306/pipocadb?"
-					+ "useTimezone=true&serverTimezone=UTC&user=root&password=rootp@ssw0rd");
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/detroit?"
+					+ "useTimezone=true&serverTimezone=UTC&user=leonardo&password=root");
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new IOException(e);
 		}
 	}
-	
-	//Conexao Gil 2
-//		public static Connection getConnection() throws IOException {
-//			try {
-//				return DriverManager .getConnection("jdbc:mysql://localhost:3306/pipocadb?"
-//						+ "useTimezone=true&serverTimezone=UTC&user=leonardo&password=root");
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//				throw new IOException(e);
-//			}
-//		}
 
 }
