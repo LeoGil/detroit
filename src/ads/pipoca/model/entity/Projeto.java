68cf9479c7977923aa24d5e6d74212928d6dbec3
Leonardo Gil <leonardo.lgc@policiacientifica.sp.gov.br>
2020-05-16 22:27:29 -0300

    resto dos atributos das classes

--- src/ads/pipoca/model/entity/Projeto.java
+++ src/ads/pipoca/model/entity/Projeto.java
@@ -7,86 +7,158 @@ public class Projeto {
 	private String nome;
 	private String descricao;
 	private Date estimativa;
-//	private Colaborador colaborador;
-//	private SituacaoProjeto situacaoProjeto;
-//	private Objetivo objetivo;
+	private Colaborador colaborador;
+	private SituacaoProjeto situacaoProjeto;
+	private Objetivo objetivo;
 	private String departamento;
 	private String resultadoEsperado;
 	private String publicoBeneficiario;
 	private Date dataCadastro;
 	private Boolean ativo;
-
+	
 	public int getId() {
 		return id;
 	}
+
+
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
+
+
 	public String getNome() {
 		return nome;
 	}
+
+
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
+
+
 	public String getDescricao() {
 		return descricao;
 	}
+
+
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
+
+
 	public Date getEstimativa() {
 		return estimativa;
 	}
+
+
 
 	public void setEstimativa(Date estimativa) {
 		this.estimativa = estimativa;
 	}
 
+
+
+	public Colaborador getColaborador() {
+		return colaborador;
+	}
+
+
+
+	public void setColaborador(Colaborador colaborador) {
+		this.colaborador = colaborador;
+	}
+
+
+
+	public SituacaoProjeto getSituacaoProjeto() {
+		return situacaoProjeto;
+	}
+
+
+
+	public void setSituacaoProjeto(SituacaoProjeto situacaoProjeto) {
+		this.situacaoProjeto = situacaoProjeto;
+	}
+
+
+
+	public Objetivo getObjetivo() {
+		return objetivo;
+	}
+
+
+
+	public void setObjetivo(Objetivo objetivo) {
+		this.objetivo = objetivo;
+	}
+
+
+
 	public String getDepartamento() {
 		return departamento;
 	}
+
+
 
 	public void setDepartamento(String departamento) {
 		this.departamento = departamento;
 	}
 
+
+
 	public String getResultadoEsperado() {
 		return resultadoEsperado;
 	}
+
+
 
 	public void setResultadoEsperado(String resultadoEsperado) {
 		this.resultadoEsperado = resultadoEsperado;
 	}
 
+
+
 	public String getPublicoBeneficiario() {
 		return publicoBeneficiario;
 	}
+
+
 
 	public void setPublicoBeneficiario(String publicoBeneficiario) {
 		this.publicoBeneficiario = publicoBeneficiario;
 	}
 
+
+
 	public Date getDataCadastro() {
 		return dataCadastro;
 	}
+
+
 
 	public void setDataCadastro(Date dataCadastro) {
 		this.dataCadastro = dataCadastro;
 	}
 
+
+
 	public Boolean getAtivo() {
 		return ativo;
 	}
 
+
+
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
 	}
+
+
 
 	@Override
 	public String toString() {
